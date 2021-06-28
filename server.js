354a6a9b8d2fc3d80c73b0e13c3250d07086872e
Raven <you@example.com>
2021-06-29 01:11:03 +0800

    snapshot

--- server.js
+++ server.js
@@ -10,65 +10,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
-  let personalityTraitsArray = [];
-  let filteredResults = animalsArray;
-  if (query.personalityTraits) {
-    if (typeof query.personalityTraits === 'string') {
-      personalityTraitsArray = [query.personalityTraits];
-    } else {
-      personalityTraitsArray = query.personalityTraits;
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      personalityTraitsArray.forEach(trait => {
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
     }
-    personalityTraitsArray.forEach(trait => {
-      filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
-      );
-    });
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
   }
-  if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
   }
-  if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
-  }
-  return filteredResults;
-}
-
-function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
-  return result;
-}
-
-function createNewAnimal(body, animalsArray) {
+  
+  function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
     fs.writeFileSync(
-      path.join(__dirname, './api/animals.json'),
+      path.join(__dirname, './data/animals.json'),
       JSON.stringify({ animals: animalsArray }, null, 2)
     );
     return animal;
   }
-
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-function validateAnimal(animal) {
+  
+  function validateAnimal(animal) {
     if (!animal.name || typeof animal.name !== 'string') {
       return false;
     }
@@ -83,11 +66,28 @@ function validateAnimal(animal) {
     }
     return true;
   }
+  
+  app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+  
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
   app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
   
-    // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
       res.status(400).send('The animal is not properly formatted.');
     } else {
@@ -95,9 +95,7 @@ function validateAnimal(animal) {
       res.json(animal);
     }
   });
-
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
   
-
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
