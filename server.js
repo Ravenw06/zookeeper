ff56911008cd7bf3c06a92dbe7002d8bbc41493d
Raven <you@example.com>
2021-07-01 01:37:44 +0800

    added route to public

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -83,7 +84,18 @@ function filterByQuery(query, animalsArray) {
       res.send(404);
     }
   });
-  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
   app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -98,4 +110,4 @@ function filterByQuery(query, animalsArray) {
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
-  
+   
